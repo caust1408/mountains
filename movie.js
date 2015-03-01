be4c31b3685e412a3456a89c4ecfd4ab47b0b6e2
Craig <cav9871@saintjoe.edu>
2015-02-28 23:16:12 -0500

    This is my movie file

---
+++ movie.js
@@ -0,0 +1,53 @@
+var MOVIE_FILE = [
+{title: "The Shawshank Redemption", rating: "9.2", genre: "crime", released: "1994",director : "Frank Darabont"},
+ {title: "The Godfather", rating: "9.2", genre: "crime", released: "1972", director: "Francis Ford Coppola"},
+ {title: "The Godfather: Part II", rating: "9.1", genre: "crime", released: "1974", director: "Francis Ford Coppola"},
+ {title: "The Dark Knight", rating: "9.0", genre: "action", released: "2008", director: "Christopher Nolan"},
+ {title: "Pulp Fiction", rating: "8.9", genre: "drama", released: "1994", director: "Quentin Tarantino"},
+ {title: "12 Angry Men", rating: "8.9", genre: "drama", released: "1957", director: "Sidney Lumet"},
+ {title: "Schindlers List", rating: "8.9", genre: "drama", released: "1993", director: "Steven Spielberg"},
+ {title: "The Good, the Bad and the Ugly", rating: "8.9", genre: "western", released: "1996", director: "Sergio Leone"},
+ {title: "The Lord of the Rings, The Return of the King", rating: "8.9", genre: "adventure", released: "2003", director: "Peter Jackson"},
+ {title: "Fight Club", rating: "8.9", genre: "drama", released: "1999", director: "David Fincher"},
+ {title: "The Lord of the Rings,The Fellowship of the Ring", rating: "8.8", genre: "adventure", released: "2001", director: "Peter Jackson"},
+ {title: "Star Wars,Episode V-The Empire Strikes Back", rating: "8.8", genre: "fantasy", released: "1980", director: "Irvin Kershner"},
+ {title: "Forrest Gump", rating: "8.8", genre: "drama", released: "1994", director: "Robert Zemeckis"},
+ {title: "Inception", rating: "8.8", genre: "mystery", released: "2010", director: "Christopher Nolan"},
+ {title: "Once Flew Over the Cuckoos Nest", rating: "8.7", genre: "drama", released: "1975", director: "Milos Forman"},
+ {title: "The Lord of the Rings,The Two Towers", rating: "8.7", genre: "adventure", released: "2002", director: "Peter Jackson"},
+ {title: "Goodfellas", rating: "8.7", genre: "crime", released: "1990", director: "Martin Scorsese"},
+ {title: "The Matrix", rating: "8.7", genre: "action", released: "1999", director: "Andy Wachowski"},
+ {title: "Star Wars, Episode IV - A New Hope", rating: "8.7", genre: "fantasy", released: "1977", director: "George Lucas"},
+ {title: "Seven Samurai", rating: "8.7", genre: "drama", released: "1954", director: "Akira Kurosawa"},
+ {title: "Interstellar", rating: "8.7", genre: "Adventure", released: "2014", director: "Christopher Nolan"},
+ {title: "City of God", rating: "8.7", genre: "drama", released: "2002", director: "Fernando Meirelles"},
+ {title: "Seven", rating: "8.7", genre: "thriller", released: "1995", director: "David Fincher"},
+ {title: "The Usual Suspects", rating: "8.7", genre: "crime", released: "1995", director: "Bryan Singer"},
+ {title: "The Silence of the Lambs", rating: "8.6", genre: "thriller", released: "1991", director: "Jonathan Demme"}
+ ];
+
+ if (typeof module != "undefined" && module.exports) 
+    module.exports = MOIVES_FILE;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
